65343c4eab9ad74f216595a6a707f84391108d66
clleung <clleung@andrew.cmu.edu>
2020-12-13 19:11:18 -0500

    updated show_all

--- show_all.sql
+++ show_all.sql
@@ -1,59 +1,56 @@
+\copy Users(username, email, country, fname, lname, join_date)     FROM 'Users.csv' csv header
+\copy Artists(artist_id, artist_name, monthly_listeners)     FROM 'Artists.csv' csv header
+\copy Sponsors(sponsor_id, sponsor_name)     FROM 'Sponsors.csv' csv header
+\copy Songs(song_name, release_date, genre, num_plays, duration, artist_id)     FROM 'Songs.csv' csv header
+
+\copy Ads(duration, frequency, information, cost, sponsor_id)     FROM 'Ads.csv' csv header
+\copy Artist_Ads(ad_id, artist_id, event_date)     FROM 'Artist_Ads.csv' csv header
+\copy External_Ads(ad_id, client_name)     FROM 'External_Ads.csv' csv header
+\copy Friends(uid1, uid2, simultaneous_play)     FROM 'Friends.csv' csv header
+\copy Stream(uid, song_id, date, time)     FROM 'Stream.csv' csv header
+
+ \! echo "Ads Table, ordered by ad_id (ascending)";
 SELECT *
   FROM Ads
  ORDER BY ad_id ASC;
 
- \! echo "Full list of users and their friends, ordered by username (ascending)";
-
+ \! echo "Artist_Ads Table, ordered by ad_id (ascending)";
 SELECT *
   FROM Artist_Ads
  ORDER BY ad_id ASC;
 
- \! echo "Full list of users and their friends, ordered by username (ascending)";
-
+ \! echo "Artist Table, ordered by artist_id (ascending)";
 SELECT *
   FROM Artists
  ORDER BY artist_id ASC;
 
- \! echo "Full list of users and their friends, ordered by username (ascending)";
-
+ \! echo "External_Ads Table, ordered by ad_id (ascending)";
 SELECT *
   FROM External_Ads
  ORDER BY ad_id ASC;
 
- \! echo "Full list of users and their friends, ordered by username (ascending)";
-
+ \! echo "Friends Table, ordered by uid1 (ascending)";
 SELECT *
   FROM Friends
  ORDER BY uid1 ASC;
 
- \! echo "Full list of users and their friends, ordered by username (ascending)";
-
-SELECT *
-  FROM Ranking
- ORDER BY sponsor_id ASC;
-
- \! echo "Full list of users and their friends, ordered by username (ascending)";
-
+ \! echo "Songs Table, ordered by song_id (ascending)";
 SELECT *
   FROM Songs
  ORDER BY song_id ASC;
 
- \! echo "Full list of users and their friends, ordered by username (ascending)";
-
+ \! echo "Sponsors Table, ordered by sponsor_id (ascending)";
 SELECT *
   FROM Sponsors
  ORDER BY sponsor_id ASC;
 
- \! echo "Full list of users and their friends, ordered by username (ascending)";
-
+ \! echo "Stream Table, ordered by stream_id (ascending)";
 SELECT *
   FROM Stream
- ORDER BY uid ASC;
+ ORDER BY ASC;
 
  \! echo "Full list of users and their friends, ordered by username (ascending)";
-
 SELECT *
   FROM Users
  ORDER BY uid ASC;
 
- \! echo "Full list of users and their friends, ordered by username (ascending)";
